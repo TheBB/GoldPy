b569e89947c333bfc57afdd960cc6b2a3b7f1062
Eivind Fonn <evfonn@gmail.com>
2021-12-28 12:12:57 +0100

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 
 class CMakeExtension(Extension):
