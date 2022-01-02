f1a5d939b46b0e31191fd04ce450b9e029691a64
Eivind Fonn <evfonn@gmail.com>
2022-01-02 21:30:30 +0100

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 class CMakeExtension(Extension):
